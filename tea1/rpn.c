9042ce63234b8fec96eb9053863319a129d8e1a3
Vitor Silva <silva.vitor@usp.br>
2022-11-26 15:42:18 +0100

    New files

---
+++ tea1/rpn.c
@@ -0,0 +1,11 @@
+#include "rpn.h"
+
+T_list s2list(char * exp){
+    // Converte expressao em linked list de T_elt (RPN_ELT)
+}
+
+T_elt rpn_eval(char * exp){
+    // Converte expressao usando a anterior
+    // Avalia a expressao da lista
+    // Implementa aqui a pilha
+}
